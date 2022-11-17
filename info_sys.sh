853bd01eba0de5f2be0acb1a5920358a2eac1ae4
RodBorneo <borneorodrigo@yahoo.com.br>
2022-11-16 22:01:18 -0300

    Retorna informações do sistema

---
+++ info_sys.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Seu nome de usuário é:"
+whoami #exibe o usuário logado
+echo "Info de hora atual e tempo que o computador está ligado:"
+uptime #exibe a hora atual e tempo descorrido desde que foi ligado
+echo "O escript está sendo executado no diretório: "
+pwd #exibe o diretório no qual o arquivo está rodando
